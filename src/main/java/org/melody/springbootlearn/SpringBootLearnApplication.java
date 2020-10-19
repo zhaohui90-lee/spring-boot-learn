64f5b386c77eab06918428612d8b7db09bd79c24
忘却的旋律 <fhenn@163.com>
2020-10-19 21:56:46 +0800

    'author'

--- src/main/java/org/melody/springbootlearn/SpringBootLearnApplication.java
+++ src/main/java/org/melody/springbootlearn/SpringBootLearnApplication.java
@@ -3,6 +3,9 @@ package org.melody.springbootlearn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author 忘却的旋律
+ */
 @SpringBootApplication
 public class SpringBootLearnApplication {
 
